75331cd8f6779d66e848c3c5a89b458de5605c8c
DenisKoriavets <d.korjavets@ukr.net>
2026-04-05 22:11:38 +0300

    Init project structure

---
+++ src/main/java/ua/edu/ukma/zlagodabackend/ZlagodaBackendApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.zlagodabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZlagodaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZlagodaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/edu/ukma/zlagodabackend/ZlagodaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.zlagodabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZlagodaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
