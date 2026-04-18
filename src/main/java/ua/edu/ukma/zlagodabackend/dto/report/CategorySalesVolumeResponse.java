b4e520956b55dc1af9489978047da00dae494e40
DenisKoriavets <d.korjavets@ukr.net>
2026-04-18 13:12:24 +0300

    feat: refactor CategorySalesVolumeResponse and VipCustomerResponse to use records

--- src/main/java/ua/edu/ukma/zlagodabackend/dto/report/CategorySalesVolumeResponse.java
+++ src/main/java/ua/edu/ukma/zlagodabackend/dto/report/CategorySalesVolumeResponse.java
@@ -1,13 +1,6 @@
 package ua.edu.ukma.zlagodabackend.dto.report;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class CategorySalesVolumeResponse {
-    private String categoryName;
-    private Long totalSoldPieces;
-}
+public record CategorySalesVolumeResponse(
+    String categoryName,
+    Long totalSoldPieces
+) {}
--- src/main/java/ua/edu/ukma/zlagodabackend/dto/report/VipCustomerResponse.java
+++ src/main/java/ua/edu/ukma/zlagodabackend/dto/report/VipCustomerResponse.java
@@ -1,14 +1,7 @@
 package ua.edu.ukma.zlagodabackend.dto.report;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class VipCustomerResponse {
-    private String cardNumber;
-    private String custSurname;
-    private String custName;
-}
+public record VipCustomerResponse(
+    String cardNumber,
+    String custSurname,
+    String custName
+) {}
